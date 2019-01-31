d3d654bb3124917ed4ac8bb9493ba579e3a6b7fc
boyle.rmb <boyle.rmb@gmail.com>
2019-01-30 19:42:29 -0500

    formatting game log

--- src/app/game-log/game-log.module.ts
+++ src/app/game-log/game-log.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameLogContainerComponent } from './game-log-container/game-log-container.component';
 import { GameLogControlsComponent } from './game-log-controls/game-log-controls.component';
+import { MatButton } from '@angular/material';
 
 @NgModule({
-  declarations: [GameLogContainerComponent, GameLogControlsComponent],
+  declarations: [GameLogContainerComponent, GameLogControlsComponent, MatButton],
   imports: [
     CommonModule
-  ]
+
+  ],
+  exports: [GameLogContainerComponent]
 })
 export class GameLogModule { }
--- src/app/tracker/tracker.module.ts
+++ src/app/tracker/tracker.module.ts
@@ -6,15 +6,16 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { TrackerListComponent } from './tracker-list/tracker-list.component';
 import { TrackerListItemComponent } from './tracker-list-item/tracker-list-item.component';
 import { TrackerContainerComponent } from './tracker-container/tracker-container.component';
-import { GameLogModule } from '../game-log/game-log.module';
 import { GameLogContainerComponent } from '../game-log/game-log-container/game-log-container.component';
+import { GameLogControlsComponent } from '../game-log/game-log-controls/game-log-controls.component';
 
 @NgModule({
   declarations: [
     TrackerContainerComponent,
     TrackerListComponent,
     TrackerListItemComponent,
-    GameLogContainerComponent
+    GameLogContainerComponent,
+    GameLogControlsComponent
   ],
   imports: [CommonModule, TrackerRoutingModule, DragDropModule],
   exports: [
